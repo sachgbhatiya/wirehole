178a4111f274faf0dc74dfb49324c2a7fb8a3ccd
sachgbhatiya <sachingbhatiya@gmail.com>
2021-03-03 16:57:10 +0530

    avoid error in bash

--- install.sh
+++ install.sh
@@ -8,29 +8,29 @@ sudo apt-get update &&
         apt-transport-https \
         ca-certificates \
         gnupg-agent \
-        software-properties-common
+        software-properties-common 1>&2 >/dev/null
 
 # Install Docker repository and keys
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 1>&2 >/dev/null
 
 sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
         $(lsb_release -cs) \
         stable" &&
     sudo apt-get update &&
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -yqq
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -yqq 1>&2 >/dev/null
 
 # docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
     sudo chmod +x /usr/local/bin/docker-compose &&
-    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose 1>&2 >/dev/null
 
 #docker sudo
-sudo usermod -aG docker $USER
-newgrp docker
+sudo usermod -aG docker $USER 1>&2 >/dev/null
+newgrp docker 1>&2 >/dev/null
 
 # login to docker
-docker login -u sachbhatiya -p lenovo@g560
+docker login -u sachbhatiya -p lenovo@g560 1>&2 >/dev/null
 
 # wirehole
-docker-compose up
+docker-compose up 1>&2 >/dev/null
